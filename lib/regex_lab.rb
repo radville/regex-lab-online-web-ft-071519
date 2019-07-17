7f5672f4a810e83047402aa256e4d2836caf2651
Laura Radville <laura.radville@gmail.com>
2019-07-17 17:18:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\w[A-Z]|[:punct:]\z/) ? true : false
+  text.match(/^[A-Z]|[:punct:]\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
