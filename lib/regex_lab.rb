2e5b7a014de8b8dec42b6b9b912864396309b448
Laura Radville <laura.radville@gmail.com>
2019-07-17 18:16:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,5 +17,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match([0-9]{4}) ? true : false
+  phone.scan([0-9]{4}) ? true : false
 end
